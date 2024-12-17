a3928557dca153dcae1caadf9ddcb58eed074eaf
Cris <152991141+crisantt@users.noreply.github.com>
2024-12-17 11:02:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,30 @@
-#!bin/bash
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
 
+mkdir ~/github
+
+## System Packages / Dependencies
+sudo apt install policykit-1-gnome network-manager-gnome network-manager libgtk-layer-shell-dev -y
+sudo apt install git build-essential -y
+clear
+
+sudo apt install sway wayland-protocols wayland-utils wl-clipboard xdg-desktop-portal-wlr libwlroots-dev xwayland qtwayland5 -y
+
+clear
+## User Packages
+sudo apt install waybar wofi mako-notifier grim slurp udiskie lxappearance feh firefox-esr -y
+
+clear
+
+## ly (Log in Manager?? Dunno what to call it)
+sudo apt install libpam0g-dev libxcb-xkb-dev
+cd ~/github
+git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly
+make
+sudo make install installsystemd
+sudo systemctl enable ly
+
+clear
+
+cd --
