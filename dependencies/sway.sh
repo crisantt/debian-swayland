9d62432c484a4c3233218ed70e367750ebdaeede
Cris <152991141+crisantt@users.noreply.github.com>
2024-12-23 10:58:27 +0800

    Update sway.sh

--- dependencies/sway.sh
+++ dependencies/sway.sh
@@ -1,1 +1,12 @@
-
+sudo apt install \
+  meson \
+  libwayland-dev \
+  wayland-protocols \
+  libpcre2-dev \
+  libjson-c-dev \
+  libpango1.0-dev \
+  libcairo2-dev \
+  libgdk-pixbuf2.0-dev \
+  swaybg \
+  scdoc \
+  git
